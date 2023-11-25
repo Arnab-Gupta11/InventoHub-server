3d34fcad1dc883ace94aa23fd11b64d64dd1bf3a
Arnab Gupta <arnabgupta1111@gmail.com>
2023-11-25 15:29:42 +0600

    Connect database with server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -9,6 +9,29 @@ const port = process.env.PORT || 5000;
 /*-------------------> parser <----------------------*/
 app.use(cors());
 app.use(express.json());
+
+/*-------------------> Connect Database <----------------------*/
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cliw5jo.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 /*-------------------> Home API <----------------------*/
 app.get("/", async (req, res) => {
